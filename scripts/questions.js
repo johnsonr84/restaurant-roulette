062be2709f7924fa6ba0b87d84f47e90c5125a65
Robert Johnson <robertcjohnson1984@gmail.com>
2021-01-23 11:34:20 -0700

    add questions

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,8 +1,21 @@
 // QUESTIONS ==============================================
-const questions = [
+const question_1 = [
 	{
 		question: 'What type of cuisine do you want?',
-		options: ['Asian', 'Italian', 'Mexican', 'American'],
-		answer: 
+		options: ['Asian', 'Italian', 'Mexican', 'American'], 
+	},
+];
+
+const question_2 = [
+	{
+		question: 'How far are you willing to travel from zip?',
+		options: ['0-0.5', '0.5-1', '1-1.5', '2+'],
+	},
+];
+
+const question_3 = [
+	{
+		question: 'What is your desired price range per person?',
+		options: ['$0-$10', '$10-15', '$15-$20', '$20+'],
 	},
 ];
