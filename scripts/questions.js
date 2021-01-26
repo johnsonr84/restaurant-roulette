ece20effd108a93f18b5eb726e3032db251c4bd6
Brandon Ashby <bash7325@gmail.com>
2021-01-25 21:09:18 -0700

    question buttons updated

--- scripts/questions.js
+++ scripts/questions.js
@@ -2,20 +2,20 @@
 const question_1 = [
 	{
 		question: 'What type of cuisine do you want?',
-		options: ['Asian', 'Italian', 'Mexican', 'American'], 
+		options: ['American', 'Italian', 'Mexican', 'Asian'], 
 	},
 ];
 
 const question_2 = [
 	{
-		question: 'How far are you willing to travel from zip?',
-		options: ['0-0.5', '0.5-1', '1-1.5', '2+'],
+		question: 'How many miles are you willing to travel from zip?',
+		options: ['0-0.5', '0.5-1', '1-2', '2-5'],
 	},
 ];
 
 const question_3 = [
 	{
 		question: 'What is your desired price range per person?',
-		options: ['$0-$10', '$10-15', '$15-$20', '$20+'],
+		options: ['$0-$10', '$10-15', '$15-$20', '$20-100'],
 	},
 ];
--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@
 $(document).ready(function () {
     var zipAPI = "3TUtnF6zFHHIVFCDd6pcO8izME9T2DA8ohrHRUZ392YILAIEFEyTHv8uK1UlVOYp"
     var zomAPI = "1f187162b82ab83fb9770f3fdc8f7124"
+    var distanceInMiles = distanceInMetres / 1609;
     //Clear localStorage every used
     localStorage.clear();
 
@@ -35,6 +36,24 @@ $(document).ready(function () {
 
             //Zomato API cuisine call
             var queryURL = "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/cuisines?lat=" + lat + "&lon=" + lng;
+
+            $.ajax({
+    
+                method: "GET",
+                crossDomain: true,
+                url: queryURL,
+                dataType: "json",
+                async: true,
+                headers: {
+                    "user-key": zomAPI
+                },
+            }).then(function (response) {
+                console.log(response);
+                
+                
+            })
+            //API call for price range
+            var queryURL = "https://cors-anywhere.herokuapp.com/hhttps://developers.zomato.com/api/v2.1/cuisines?res_id=average_cost_for_two";
 
             $.ajax({
     
